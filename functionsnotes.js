2e649e60bf64f43b701d647373ce4cee056841ad
8eel <8eel@fresnou.org>
2023-03-14 13:58:40 -0700

    initial commit

---
+++ functionsnotes.js
@@ -0,0 +1,39 @@
+//Functions Notes
+//Step 1 - Define the function
+// function sayHello(){
+//     console.log("hello");
+// }
+//Step 2 - call (use) the function
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+
+// function printmax(){
+//     let x = 6;
+//     let y = 29;
+//     if(x > y){
+//         console.log(`the larger number is ${x}`);
+//     } else {
+//         console.log(`the larger number is ${y}`);
+//     }
+// }
+//
+// printmax();
+
+// function printmax(x, y){
+//     if(x > y){
+//         console.log(`the larger number is ${x}`);
+//     } else {
+//         console.log(`the larger number is ${y}`);
+//     }
+// }
+//
+// printmax(9, 12);
+
+function sum(number1, number2){
+    console.log(`${number1} + ${number2} = ${number1+number2}`);
+}
+
+sum(8,9);
+sum(9,0);
