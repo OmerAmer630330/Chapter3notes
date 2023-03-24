ecc2d1b996d81aef317de725007079ecdf269125
8eel <8eel@fresnou.org>
2023-03-24 14:18:23 -0700

    update

--- arrays1.js
+++ arrays1.js
@@ -1,3 +1,13 @@
 //1
 let mynum=[1,2,3,4];
-console.log(mynum)
+console.log(mynum);
+//2
+let num1 = ['1', '2', '3', '4', '5', '6'];
+console.log(num1[5]);
+//3
+num2=[0] = [];
+console.log(num2[0]);
+//4
+function RE(arr) {
+    for
+}
