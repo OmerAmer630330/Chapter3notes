428991ff504c65973c3c490fd2c3eb97ac5891ed
8eel <8eel@fresnou.org>
2023-04-11 14:14:33 -0700

    madLibs

---
+++ madLibs.js
@@ -0,0 +1,13 @@
+const readline=require('readline-sync');
+
+let word= readline.question("adjective:");
+let word2= readline.question("adjective:");
+let word3= readline.question("place:");
+let word4= readline.question("adjective:");
+let word5= readline.question("place:");
+let word6= readline.question("emotion:");
+let word7= readline.question("adjective:");
+let word8= readline.question("adjective:");
+let word9= readline.question("adjective:");
+let word10= readline.question("adjective:");
+let word11= readline.question("noun:");
