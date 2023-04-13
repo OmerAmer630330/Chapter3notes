0c43b3cb4787d7c78acd93a171062531abe4cdd5
8eel <8eel@fresnou.org>
2023-04-13 13:58:31 -0700

    8ball2

--- 8ball.js
+++ 8ball.js
@@ -1,5 +1,8 @@
+const readline=require('readline-sync');
+
 let answers=['yes', 'no', 'maybe', 'probably', 'probably not', 'definitely', 'of course', 'probably not', 'never'];
 
 let randomNum = Math.floor(Math.random()*answers.length);
 let answer = answers[randomNum];
+let word = readline.question("ask a question:");
 console.log(answer);
